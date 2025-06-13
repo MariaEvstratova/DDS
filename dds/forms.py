9aa215a329b7b0b6466a632c0dd2a5cc98261c99
mariaevstratova <evstratova.mashenka@gmail.com>
2025-06-13 15:03:12 +0500

    Adding forms for notes

--- dds/forms.py
+++ dds/forms.py
@@ -55,3 +55,60 @@ class MoneyOperationForm(forms.Form):
             self.fields['subcategory'].initial = instance.subcategory
             self.fields['amount'].initial = instance.amount
             self.fields['comment'].initial = instance.comment
+
+
+# Форма добавления статуса
+class StatusForm(forms.Form):
+    status = forms.CharField(
+        label='Статус'
+    )
+
+
+# Форма добавления типа операции
+class OperationTypeForm(forms.Form):
+    operation_type = forms.CharField(
+        label='Тип операции'
+    )
+
+
+# Форма добавления категории
+class CategoryForm(forms.Form):
+    category = forms.CharField(
+        label='Категория'
+    )
+
+    operation_type = forms.ModelChoiceField(
+        queryset=OperationType.objects.all(),
+        label='Тип операции'
+    )
+
+    # Обновление данных из instance
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.pop('instance', None)
+        super().__init__(*args, **kwargs)
+
+        if instance:
+            self.fields['category'].initial = instance.category
+            self.fields['operation_type'].initial = instance.operation_type
+
+
+# Форма добавления подкатегории
+class SubCategoryForm(forms.Form):
+    subcategory = forms.CharField(
+        label='Подкатегория'
+    )
+
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        label='Категория'
+    )
+
+    # Обновление данных из instance
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.pop('instance', None)
+        super().__init__(*args, **kwargs)
+
+        if instance:
+            self.fields['subcategory'].initial = instance.subcategory
+            self.fields['category'].initial = instance.category
+
