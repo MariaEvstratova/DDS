b97de51667f87329d00bc89a42ea076f22bf3239
mariaevstratova <evstratova.mashenka@gmail.com>
2025-06-13 15:53:59 +0500

    Creating documentation README.md

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # DDS
+# Веб-сервис для управления движением денежных средств (ДДС)
+
+[![Python 3.8+](https://img.shields.io/badge/python-3.8+-blue.svg)](https://www.python.org/downloads/)
+[![Django](https://img.shields.io/badge/django-4.0-brightgreen.svg)](https://www.djangoproject.com/)
+
+## 🛠 Установка зависимостей
+
+```bash
+# Клонирование репозитория
+git clone https://github.com/ваш-username/название-репозитория.git
+cd название-репозитория
+
+# Создание виртуального окружения
+python -m venv venv  # Windows/Linux
+source venv/bin/activate  # Linux/MacOS
+venv\Scripts\activate  # Windows
+
+# Установка зависимостей
+pip install -r requirements.txt
+```
+
+## 🗃 Настройка базы данных
+
+SQLite (по умолчанию)
+```bash
+python manage.py migrate
+```
+
+## 🚀 Запуск веб-сервиса
+
+```bash
+python manage.py runserver
+```
+
+## 🌐 Доступные URL
+
+Основное приложение: http://127.0.0.1:8000
+Админ-панель: http://127.0.0.1:8000/admin
+API документация: http://127.0.0.1:8000/api/docs/
--- dds_settings/urls.py
+++ dds_settings/urls.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
 from django.urls import path
-from dds import views
+from . import views
 
 
 # Добавление url путей
 urlpatterns = [
     path("", views.index),
+    path("dicts", views.dicts),
     path("edit_operation/<int:id>", views.edit_operation),
     path("add_operation", views.add_operation),
     path("delete_operation/<int:id>", views.delete_operation),
+    path("edit_status/<int:id>", views.edit_status),
+    path("add_status", views.add_status),
+    path("delete_status/<int:id>", views.delete_status),
+    path("edit_operation_type/<int:id>", views.edit_operation_type),
+    path("add_operation_type", views.add_operation_type),
+    path("delete_operation_type/<int:id>", views.delete_operation_type),
+    path("edit_category/<int:id>", views.edit_category),
+    path("add_category", views.add_category),
+    path("delete_category/<int:id>", views.delete_category),
+    path("edit_subcategory/<int:id>", views.edit_subcategory),
+    path("add_subcategory", views.add_subcategory),
+    path("delete_subcategory/<int:id>", views.delete_subcategory),
     path('admin/', admin.site.urls),
 ]
